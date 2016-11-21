7cb507cfe35262d0a02dbdd52c38635f9c933658
mohammad <shalabimobile@gmail.com>
2016-11-21 08:53:43 +0100

    creating EventTracker module.

---
+++ Pluralsight/EventTracker/src/main/java/com/mvc/EventTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTrackerApplication.class, args);
+	}
+}
