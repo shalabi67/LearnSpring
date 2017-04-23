6bd257e572a136cef69f30db9cc17a6345968fa1
mohammad <shalabimobile@gmail.com>
2017-04-23 18:51:44 +0200

    getting principal in controller

--- LearnSpringSecurity/src/main/java/com/learn/security/controller/AdminController.java
+++ LearnSpringSecurity/src/main/java/com/learn/security/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.learn.security.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class AdminController {
 	
 	@RequestMapping(value="/admin", method=RequestMethod.GET )
-	public String login(ModelMap model) {
+	public String login(ModelMap model, Principal principal) {
+		
+		System.out.println(principal.getName());
 		return "admin";
 	}
 	
