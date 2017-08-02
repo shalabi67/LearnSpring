fdcfe0ebcde9e99fe3bbc9112a78cec4e2b278c3
mshalabi <mohamad.shalabi@mytoys.de>
2017-08-02 16:39:56 +0200

    adding inital sql data to h2 database.

---
+++ LearnJpa/src/main/resources/data-my_h2.sql
@@ -0,0 +1,3 @@
+INSERT INTO ADDRESS ( CITY , POST_CODE , STREET) VALUES ( 'berlin', '10315', 'löwenberger str. 2');
+INSERT INTO ADDRESS ( CITY , POST_CODE , STREET) VALUES ( 'berlin', '11315', 'str 10');
+INSERT INTO ADDRESS ( CITY , POST_CODE , STREET) VALUES ( 'frankfort', '10315', 'löwenberger str. 2');
