1a3cf8131e19ff38e6a2913425049a681caea47e
mshalabi <mohamad.shalabi@mytoys.de>
2017-10-13 17:50:47 +0200

    discovery service example including server, client and service.

---
+++ LearnCloud/discovery-service-client/src/test/java/com/learn/cloud/discovery_service_client/DiscoveryServiceClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.cloud.discovery_service_client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryServiceClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
