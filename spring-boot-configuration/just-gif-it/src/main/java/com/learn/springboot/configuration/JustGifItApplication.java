47b519fedc0d6c3f93c6aa6aa79858dc0027a765
mshalabi <mohamad.shalabi@mytoys.de>
2017-10-20 17:26:13 +0200

    DEMO: disable complet ato configuration using @SpringBootApplication

--- spring-boot-configuration/just-gif-it/src/main/java/com/learn/springboot/configuration/JustGifItApplication.java
+++ spring-boot-configuration/just-gif-it/src/main/java/com/learn/springboot/configuration/JustGifItApplication.java
@@ -3,6 +3,9 @@ package com.learn.springboot.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +14,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import javax.annotation.PostConstruct;
 import java.io.File;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class,
+        JmxAutoConfiguration.class,
+        WebSocketAutoConfiguration.class})
 public class JustGifItApplication {
 
     @Value("${multipart.location}/gif/")
