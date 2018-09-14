a22b1491ecb4c0a362ef237cec56c82696baba7b
mshalabi <mohamad.shalabi@mytoys.de>
2018-09-14 15:32:45 +0200

    initial project

---
+++ learnmvc/src/main/java/com/learnspring/learnmvc/LearnmvcApplication.java
@@ -0,0 +1,12 @@
+package com.learnspring.learnmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnmvcApplication.class, args);
+	}
+}
