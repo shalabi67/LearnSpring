a50ef8a3cf0127f64724b45edf9716a56629acdf
mshalabi <mohamad.shalabi@mytoys.de>
2018-09-24 14:37:12 +0200

    adding default profile support

--- SpringGuru/dependency-injection/src/main/java/com/learn/dependency_injection/profiles/services/HttpService.java
+++ SpringGuru/dependency-injection/src/main/java/com/learn/dependency_injection/profiles/services/HttpService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("http")
+@Profile({"http", "default"})
 public class HttpService implements CommunicationInterface {
 
 	@Override public String getProtocol() {
