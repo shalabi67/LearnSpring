7b4498cbe47720763ccc1e22181e8d0c571e644b
mshalabi <mohamad.shalabi@mytoys.de>
2018-09-28 16:12:49 +0200

    fixing hello world application

--- SpringGuru/dependency-injection/src/main/java/com/learn/dependency_injection/components/HelloWorldApplication.java
+++ SpringGuru/dependency-injection/src/main/java/com/learn/dependency_injection/components/HelloWorldApplication.java
@@ -1,15 +1,14 @@
-package com.learn.dependency_injection;
+package com.learn.dependency_injection.components;
 
-import com.learn.dependency_injection.components.HelloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class DependencyInjectionApplication {
+public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(DependencyInjectionApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(HelloWorldApplication.class, args);
 		HelloWorld helloWorld = (HelloWorld)applicationContext.getBean("helloWorld");
 		helloWorld.show();
 	}
