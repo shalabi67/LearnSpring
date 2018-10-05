73b1ccae2dab7664a16905a790b4dd1db72d73e1
mshalabi <mohamad.shalabi@mytoys.de>
2018-10-05 12:16:21 +0200

    recipe: adding methods to repository.

--- SpringGuru/recipe/src/main/java/com/learn/spring/recipe/repositories/CategoryRepository.java
+++ SpringGuru/recipe/src/main/java/com/learn/spring/recipe/repositories/CategoryRepository.java
@@ -4,7 +4,9 @@ import com.learn.spring.recipe.models.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Long>{
-
+	Optional<Category> findByName(String name);
 }
--- SpringGuru/recipe/src/main/java/com/learn/spring/recipe/repositories/UnitOfMeasurementRepository.java
+++ SpringGuru/recipe/src/main/java/com/learn/spring/recipe/repositories/UnitOfMeasurementRepository.java
@@ -4,7 +4,10 @@ import com.learn.spring.recipe.models.UnitOfMeasurement;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UnitOfMeasurementRepository extends CrudRepository<UnitOfMeasurement, Short>{
 
+	Optional<UnitOfMeasurement> findByName(String name);
 }
