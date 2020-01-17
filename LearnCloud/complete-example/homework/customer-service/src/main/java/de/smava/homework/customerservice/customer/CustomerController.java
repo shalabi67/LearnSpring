71ae8d5176bfb2b9f2e19be3da71e978b6f7b7ba
Mohammad Shalabi <shalabicloud@gmail.com>
2020-01-17 20:41:27 +0100

    add customer service

---
+++ LearnCloud/complete-example/homework/customer-service/src/main/java/de/smava/homework/customerservice/customer/CustomerController.java
@@ -0,0 +1,24 @@
+package de.smava.homework.customerservice.customer;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(CustomerController.CUSTOMER_URL)
+public class CustomerController {
+    public static final String CUSTOMER_URL = "/api/customers";
+
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
+        return customerService.addCustomer(customer);
+    }
+}
