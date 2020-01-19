cad4a4494ea935d8ead37ae07e106c5d1481e828
mohammad <shalabi67@hotmail.com>
2020-01-19 19:07:48 +0100

    adding create customer

--- LearnCloud/complete-example/homework/customer-service/src/main/java/de/smava/homework/customerservice/customer/CustomerController.java
+++ LearnCloud/complete-example/homework/customer-service/src/main/java/de/smava/homework/customerservice/customer/CustomerController.java
@@ -1,18 +1,16 @@
 package de.smava.homework.customerservice.customer;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(CustomerController.CUSTOMER_URL)
+@RequiredArgsConstructor
 public class CustomerController {
     public static final String CUSTOMER_URL = "/api/customers";
 
-    private CustomerService customerService;
-
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+    private final CustomerService customerService;
 
     @PostMapping
     public ResponseEntity<Customer> addCustomer(
--- LearnCloud/complete-example/homework/customer-service/src/main/java/de/smava/homework/customerservice/customer/CustomerService.java
+++ LearnCloud/complete-example/homework/customer-service/src/main/java/de/smava/homework/customerservice/customer/CustomerService.java
@@ -2,13 +2,16 @@ package de.smava.homework.customerservice.customer;
 
 import de.smava.homework.customerservice.user.UserClient;
 import de.smava.homework.customerservice.user.UserDTO;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-
+/*
+eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsibWljcm8iXSwidXNlcl9uYW1lIjoiam9obiIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdLCJleHAiOjE1Nzk0NTEyNTksImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJqdGkiOiI3OWIwMmU2Ny04ZjVhLTRiMDQtYWIxZC1jNWZkZDQwOGQxMzMiLCJjbGllbnRfaWQiOiJjbGllbnQifQ.HqJ5wXLOdOZC-c2txzJxqDzdGGgai2tzehRv5j_GvHNA3T0rUFn-S6fbmgD1PcuNO4vDiZEG6tyvRJKxqnRJ3YjLE7G-ri0FxwlyeCsgl-apLoO-DqhR7lJIsH3xJ_shj8qkimyzBaaa4AwF_0Tl3Prf-uakXNUNqdpOIKezcO6Q6QzDtDwmbhT8VC5QWwMWYpruT6QKgM_MsrRyer44Syz6ogBLCklmNQ9xkiMNlCsOLVgegx-kYwaMBaX74IGCLHsHf4vslUmIeGqRZaD7bV44BSmui618dOB7xABZqZ3LfwJho6gE1BHNnTqks0FQwqf-HuxWdSLtCmqhgw_n9Q
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -17,23 +20,36 @@ public class CustomerService {
     private final UserClient userClient;
 
     public ResponseEntity<Customer> addCustomer(String token, Customer customer) {
-        if (!validateInputData(customer)) {
+        if (hasInvalidInputData(customer)) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        try {
-            ResponseEntity<UserDTO> userResponse = userClient.getUser(token, customer.getUserId());
-            if (userResponse == null || userResponse.getStatusCode() != HttpStatus.OK) {
-                return new ResponseEntity("User does not exist", HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity("User does not exist", HttpStatus.NOT_FOUND);
+        ResponseEntity userResponse = getUser(token, customer);
+        if(userResponse.getStatusCode() != HttpStatus.OK) {
+            return userResponse;
         }
 
         return saveCustomer(customer);
     }
 
-    private boolean validateInputData(Customer customer) {
+    private ResponseEntity<UserDTO> getUser(String token, Customer customer){
+        try {
+            ResponseEntity<UserDTO> userResponse = userClient.getUser(token, customer.getUserId());
+            if (userResponse == null || userResponse.getStatusCode() != HttpStatus.OK) {
+                return getUserDoesNotExistResponse();
+            }
+            return userResponse;
+        } catch(FeignException feignException) {
+            if(feignException.status() == 401) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+            return getUserDoesNotExistResponse();
+        } catch (Exception e) {
+            return getUserDoesNotExistResponse();
+        }
+
+    }
+    private boolean hasInvalidInputData(Customer customer) {
         return customer == null ||
                 StringUtils.isEmpty(customer.getFirstName()) ||
                 StringUtils.isEmpty(customer.getLastName()) ||
@@ -41,10 +57,15 @@ public class CustomerService {
                 StringUtils.isEmpty(customer.getUserId());
     }
 
+    private ResponseEntity getUserDoesNotExistResponse() {
+        return new ResponseEntity("User does not exist", HttpStatus.NOT_FOUND);
+    }
+
     private ResponseEntity<Customer> saveCustomer(Customer customer) {
         customer.setId(null);
         try {
-            return ResponseEntity.ok(customerRepository.save(customer));
+            Customer newCustomer =  customerRepository.save(customer);
+            return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
         }catch(Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
